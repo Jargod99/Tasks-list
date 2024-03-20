fee327cea73936a2013b688e40918815e449c510
Jaroslaw Godlewski <jgodlewski99@gmail.com>
2024-03-20 20:09:07 +0100

    add render function

--- script/script.js
+++ script/script.js
@@ -3,14 +3,25 @@
         content: "test",
         done: true,
     },
-{
-    content: "test2",
-    done: false,
-}];
+    {
+        content: "test2",
+        done: false,
+    }];
 
+    const render = () => {
+        let addTextToHtml = "";
+        const tasksElement = document.querySelector(".js-tasks");
+        for (const task of tasks) {
+            addTextToHtml += `
+        <li>
+        ${task.content}
+        </li>`
+        };
+        tasksElement.innerHTML = addTextToHtml;
+    };
 
     const init = () => {
-
+        render();
     };
     init();
 }
